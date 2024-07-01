d292547f65d7b0352355c7846ab0f49d970664f6
Eunsong Koh <eunsongkohh@gmail.com>
2024-06-30 18:27:51 -0700

    dnsWeb/Eunsong - Navbar now visible

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import * as React from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Nav from "./components/Nav/Nav";
 import "./index.css";
+import Layout from "./Layout";
 import ContactPage from "./pages/Contact/ContactPage";
 import HomePage from "./pages/Home/HomePage";
 import ProjectsPage from "./pages/Projects/ProjectsPage";
@@ -9,24 +9,22 @@ import ProjectsPage from "./pages/Projects/ProjectsPage";
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <HomePage />,
-  },
-  {
-    path: "/contact",
-    element: <ContactPage />,
-  },
-  {
-    path: "/projects",
-    element: <ProjectsPage />,
+    element: <Layout />,
+    children: [
+      { path: "/", element: <HomePage /> },
+      { path: "/contact", element: <ContactPage /> },
+      {
+        path: "/projects",
+        element: <ProjectsPage />,
+      },
+    ],
   },
 ]);
 
 function App() {
   return (
     <React.StrictMode>
-      <RouterProvider router={router}>
-        <Nav />
-      </RouterProvider>
+      <RouterProvider router={router}></RouterProvider>
     </React.StrictMode>
   );
 }
--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,13 +1,9 @@
-import React from 'react';
+import React from "react";
 import { Link } from "react-router-dom";
-import './Nav.css';
+import "./Nav.css";
 
 function Nav() {
   return (
-    // <header className={classes.header}>
-    //   <div className={classes.logo}>
-    //     &#123;<div className={classes.dns}>dns</div>
-    //   </div>
     <header>
       <nav>
         <ul>
