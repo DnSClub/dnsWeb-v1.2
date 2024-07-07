4e6c11a246d0b042d00eda1b7ab00b88e296c32e
Mightyy <59546774+Mighty303@users.noreply.github.com>
2024-07-06 20:08:11 -0700

    fixed navbar

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,12 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "./Nav.css";
 
 function Nav() {
   return (
     <header>
       <nav>
-        <ul>
+        <ul className='flex justify-end gap-12 p-6 text-white text-2xl'>
           <li>
             <Link to="/">home</Link>
           </li>
