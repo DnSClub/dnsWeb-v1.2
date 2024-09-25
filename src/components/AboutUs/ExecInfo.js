0f26279ecd0d03b3e665dd25bb6e2341435a5de3
Sophia Don Tranho <66497192+sophiadt@users.noreply.github.com>
2024-09-24 23:20:50 -0700

    update bios

--- src/components/AboutUs/ExecInfo.js
+++ src/components/AboutUs/ExecInfo.js
@@ -7,7 +7,7 @@ export const data = [
     "Arya",
     "https://www.linkedin.com/in/aryafaghihy/",
     "mfa90@sfu.ca",
-    "Hi! I’m Arya, a second-year computing science student here at SFU.I am passionate about cybersecurity, A.I., machine learning, networking as well as full-stack web development. Since the age of 13, I’ve spent a large amount of my time delving into these topics while participating in hackathons, game jams and coding competitions to test my skills and keep improving. My go-to programming languages are C++, Python, C, Java & Javascript. I’ve been the recipient of numerous accolades for my academic & volunteer work, demonstrating my commitment to excellence as well as my teamwork & leadership skills. Feel free to reach out to me on Email, LinkedIn or on Discord!",
+    "Hi! I’m Arya, a third-year computing science student here at SFU.I am passionate about cybersecurity, A.I., machine learning, networking as well as full-stack web development. Since the age of 13, I’ve spent a large amount of my time delving into these topics while participating in hackathons, game jams and coding competitions to test my skills and keep improving. My go-to programming languages are C++, Python, C, Java & Javascript. I’ve been the recipient of numerous accolades for my academic & volunteer work, demonstrating my commitment to excellence as well as my teamwork & leadership skills. Feel free to reach out to me on Email, LinkedIn or on Discord!",
     null,
     "images/arya.png"
   ),
@@ -16,7 +16,7 @@ export const data = [
     "Cohen",
     "https://www.linkedin.com/in/cohen-ter-heide-a12729272/",
     "cjt15@sfu.ca",
-    "Hello, I'm a second year CS student at SFU! My coding journey began at the age of 8 on Khan Academy, and I've since become an enthusiast of programming language development. In 2018, I received the prestigious PARC Systems Award for my efforts. I also have hands-on experience as a former developer at MindBlocks Studio, LLC, where I contributed to a video game with over 10 million downloads on Google Play, 1 million on iOS, and 20,000 on Steam.",
+    "Hello, I'm a third year CS student at SFU! My coding journey began at the age of 8 on Khan Academy, and I've since become an enthusiast of programming language development. In 2018, I received the prestigious PARC Systems Award for my efforts. I also have hands-on experience as a former developer at MindBlocks Studio, LLC, where I contributed to a video game with over 10 million downloads on Google Play, 1 million on iOS, and 20,000 on Steam.",
     null,
     "images/cohen.png"
   ),
@@ -34,7 +34,7 @@ export const data = [
     "Saarang",
     "https://www.linkedin.com/in/saaranganand/",
     "saa183@sfu.ca",
-    "Hi! I'm Saarang, a second-year CS student at SFU. I'm passionate about both front and back-end development, and I have a special interest in game design. You'll often find me at hackathons and tech club events, whether as a participant or organizer. Beyond technology, I'm also fascinated by the latest advancements in science and space exploration.",
+    "Hi! I'm Saarang, a third-year CS student at SFU. I'm passionate about both front and back-end development, and I have a special interest in game design. You'll often find me at hackathons and tech club events, whether as a participant or organizer. Beyond technology, I'm also fascinated by the latest advancements in science and space exploration.",
     null,
     "images/saraang.png"
   ),
@@ -43,7 +43,7 @@ export const data = [
     "Sophia",
     "https://www.linkedin.com/in/sophiadontranho/",
     "syd3@sfu.ca",
-    "My name's Sophia and I'm a second-year studying computer science at Simon Fraser University. I'm still exploring but I have an interest in software development, game development, AI, and UI/UX design. 💻 I have been heavily involved in workshops, hackathons, and projects to broaden and strengthen my skills. In my leisure, I tinker with my projects, such as my AI Discord chat bots.",
+    "My name's Sophia and I'm acomputer science student at Simon Fraser University. I'm still exploring but I have an interest in software development, game development, AI, and UI/UX design. 💻 I have been heavily involved in workshops, hackathons, and projects to broaden and strengthen my skills. In my leisure, I tinker with my projects, such as my AI Discord chat bots.",
     null,
     "images/sophia.jpg"
   ),
@@ -52,7 +52,7 @@ export const data = [
     "Sam",
     "https://www.linkedin.com/in/shanglin-zhuang-014487262/",
     "sza193@sfu.ca",
-    "Hello, I'm Shanglin, a second-year Computing Science student at SFU, and I proudly serve as the Director of Resources for the Developers and System Club (DNS). Within the tech world, my passion lies in harnessing AI to boost daily productivity for everyone. Beyond coding and algorithms, I find my creative outlet in playing metal guitar. This combination of interests makes me a unique addition to DNS.",
+    "Hello, I'm Shanglin, a third-year Computing Science student at SFU, and I proudly serve as the Director of Resources for the Developers and System Club (DNS). Within the tech world, my passion lies in harnessing AI to boost daily productivity for everyone. Beyond coding and algorithms, I find my creative outlet in playing metal guitar. This combination of interests makes me a unique addition to DNS.",
     null,
     "images/sam.jpg"
   ),
